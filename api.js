cf7aecbcbfa5908df31e8efb612a1fb50b97a692
Huy <115209569+PhamNhatHuy711@users.noreply.github.com>
2024-11-15 00:14:59 +0700

    Add files via upload

---
+++ api.js
@@ -0,0 +1,50 @@
+// const fetch = require('node-fetch');
+
+// const apiUrl = 'https://api.gcalls.co/api/v1/readAllAgents';
+// const apiKey = '8fbf78106f7c40d880fcf6d4a6d88b46'; // Thay thế bằng API key thực tế
+
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'text/csv',
+//         'x-apikey': apiKey,
+//     }
+// };
+
+// fetch(apiUrl, options)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error(`Network response was not ok (status ${response.status})`);
+//         }
+//         return response.text();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('Error fetching data:', error);
+//     });
+const apiUrl = 'https://api.gcalls.co/api/v1/readAllAgents';
+const apiKey = '8fbf78106f7c40d880fcf6d4a6d88b46'; // Thay thế bằng API key thực tế
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Accept': 'text/csv',
+        'x-apikey': apiKey,
+    }
+};
+
+fetch(apiUrl, options)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok (status ${response.status})`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
